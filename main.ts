abaddde2ddc1fdeead6b3c4d7110b6dee93e281b
Fatouseck06 <125355360+Fatouseck06@users.noreply.github.com>
2023-03-21 10:08:50 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,29 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
     	
-    } else if (false) {
-    	
+    } else if (Juego == 1) {
+        basic.showIcon(IconNames.Happy)
+    } else if (Juego == 1) {
+        basic.showIcon(IconNames.Asleep)
+    } else {
+        basic.showIcon(IconNames.Sad)
     }
+    if (receivedNumber == 2) {
+    	
+    } else if (Juego == 2) {
+        basic.showIcon(IconNames.Asleep)
+    } else if (Juego == 1) {
+        basic.showIcon(IconNames.Happy)
+    }
+    basic.showIcon(IconNames.Sad)
+    if (receivedNumber == 3) {
+    	
+    } else if (Juego == 2) {
+        basic.showIcon(IconNames.Sad)
+    } else if (Juego == 1) {
+        basic.showIcon(IconNames.Happy)
+    }
+    basic.showIcon(IconNames.Asleep)
 })
 input.onGesture(Gesture.Shake, function () {
     Juego = randint(1, 3)
